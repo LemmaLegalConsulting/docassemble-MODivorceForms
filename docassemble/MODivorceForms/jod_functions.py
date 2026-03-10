3eae03b0b09cd29ea2809494043f26c24117b2df
Toby Fey <tobyfey@protonmail.com>
2026-03-10 11:33:37 -0400

    added progressive disclosure

---
+++ docassemble/MODivorceForms/jod_functions.py
@@ -0,0 +1,15 @@
+from docassemble.base.functions import states_list
+from docassemble.base.util import Address, validation_error
+
+
+def validate_us_state(address: Address) -> None:
+    if (not hasattr(address,'country') or address.country == "US") and hasattr(address,'state') and not len(address.state) == 2:
+        users_state = address.state.lower()
+        states_abbr = {v.lower(): k for k, v in states_list().items()}
+        if users_state in states_abbr:
+            address.state = states_abbr[users_state]
+        else:
+            validation_error("You must enter the state's two-letter abbreviation.", f"""{address.attr_name("state")}""")
+
+def not_name_change(case):
+    return case.type != "name_change"
